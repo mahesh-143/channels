49dc9485aa23fb44c6ff0dd275539fbb8f49510d
mahesh-143 <maheshodedara13@yahoo.com>
2024-04-05 19:48:35 +0530

    check if email already exist

--- server/http-server/main.go
+++ server/http-server/main.go
@@ -28,6 +28,18 @@ func handleCreate(w http.ResponseWriter, r *http.Request) {
 	}
 	defer r.Body.Close()
 
+	var existingUser User
+	query := "SELECT user_id FROM users WHERE email = ? LIMIT 1 ALLOW FILTERING"
+	if err := Session.Query(query, newUser.Email).Scan(&existingUser.UserID); err == nil {
+		http.Error(w, "Email already exists", http.StatusConflict)
+		log.Println("Email already exists in the database")
+		return
+	} else if err != gocql.ErrNotFound {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		log.Println("Error querying database:", err)
+		return
+	}
+
 	newUser.CreatedAt = time.Now()
 	newUser.UserID = gocql.TimeUUID()
 
@@ -39,7 +51,7 @@ func handleCreate(w http.ResponseWriter, r *http.Request) {
 	}
 	newUser.Password = string(hashedPassword)
 
-	query := "INSERT INTO users (user_id, username, email, password, created_at) VALUES (?, ?, ?, ?, ?)"
+	query = "INSERT INTO users (user_id, username, email, password, created_at) VALUES (?, ?, ?, ?, ?)"
 
 	if err := Session.Query(query, newUser.UserID, newUser.Username, newUser.Email, newUser.Password, newUser.CreatedAt).Exec(); err != nil {
 		log.Println("Error while inserting into database:", err)
